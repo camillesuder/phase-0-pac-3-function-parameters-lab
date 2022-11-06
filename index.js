9af1a17ac093d6c45e1430274d27009d236dd5d8
Camille Suder <camillesuder@gmail.com>
2022-11-05 18:57:53 -0700

    commit tree d50ccc431c273efae3c5cfcede33d87f9621722c

---
+++ index.js
@@ -0,0 +1,11 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}`);
+}
+
+function introductionwithLanguageOptional(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+}
